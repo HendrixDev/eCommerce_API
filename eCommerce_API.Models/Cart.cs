5d7e17b078ede40b111de50f4fb5d69a8ba7a8a6
Zack Hendrix <m374lf0rlyf3@hotmail.com>
2025-03-14 22:34:58 -0500

    Added scaffolded product controller and cart controller

---
+++ eCommerce_API.Models/Cart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce_API.Models
+{
+    public class Cart
+    {
+        public List<Product> ShoppingCart { get; set; }
+        public decimal CartTotal { get; set; }
+    }
+}
---
+++ eCommerce_API/Controllers/CartController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+
+
+
+
+    }
+}
